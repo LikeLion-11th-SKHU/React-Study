c7710243f315290fb99cf96af86ddef6f7fdeb16
gracekim527 <lemonherb0323@naver.com>
2023-09-18 23:19:52 +0900

    :white_check_mark: [Add] Section 2-4

---
+++ Gracekim/Ch02/ch02_4/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import P from './P'
+
+export default function App() {
+  const texts = ['hello', 'world'].map((text, index) =>
+  <P key={index} children={text} />)
+  return <div children={texts} />
+}
