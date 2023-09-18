58af7fd03e1bd543c72190917ffd3831576f2df9
gracekim527 <lemonherb0323@naver.com>
2023-09-18 23:06:57 +0900

    :white_check_mark: [Add] Section 2-3

---
+++ Gracekim/Ch02/ch02_3/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ClassComponent from './ClassComponent';
+import ArrowComponent from './ArrowComponent';
+
+export default function App() {
+  return (
+    <ul>
+      <ClassComponent href="http://www.google.com" text="go to Google" />
+      <ArrowComponent href="https://twitter.com" text="go to Twitter" />
+    </ul>
+  )
+}
