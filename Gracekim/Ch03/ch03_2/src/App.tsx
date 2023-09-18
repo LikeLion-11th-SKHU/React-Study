6b12f1003503884fd978b311a9674727be14deda
gracekim527 <lemonherb0323@naver.com>
2023-09-19 01:52:33 +0900

    :white_check_mark: [Add] Section 3-2

---
+++ Gracekim/Ch03/ch03_2/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tailwindcss from './pages/Tailwindcss'
+import Color from './pages/Color'
+import TextsTest from './pages/TextsTest'
+
+export default function App() {
+  return (
+    <div>
+      <TextsTest />
+      <Color />
+      <Tailwindcss />
+    </div>
+  )
+}
