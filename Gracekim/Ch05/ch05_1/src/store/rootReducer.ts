23dc54d1bc17e528fbd5234ddeb1a79d53316e5f
gracekim527 <lemonherb0323@naver.com>
2023-11-03 03:05:04 +0900

    :white_check_mark: [Add] Section 5-1

---
+++ Gracekim/Ch05/ch05_1/src/store/rootReducer.ts
@@ -0,0 +1,17 @@
+import type { AppState } from "./AppState";
+import type { Actions } from "./actions";
+
+const initialAppState = {
+  today: new Date(),
+};
+export const rootReducer = (
+  state: AppState = initialAppState,
+  action: Actions
+) => {
+  switch (action.type) {
+    case "setToday": {
+      return { ...state, today: action.today };
+    }
+  }
+  return state;
+};
